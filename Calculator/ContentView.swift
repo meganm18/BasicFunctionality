57424e6b4366b511a15e5ebc3e699a273457d000
meganm18 <mem5ak@virginia.edu>
2020-07-10 19:13:52 -0400

    Initial Commit

---
+++ Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Megan Marshall on 7/10/20.
+//  Copyright © 2020 Megan Marshall. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
