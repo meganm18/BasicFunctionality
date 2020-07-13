44945d09edeb6c1538ca49dfc863875272ad6d78
meganm18 <mem5ak@virginia.edu>
2020-07-13 16:34:12 -0400

    Basic Format but no functionality.

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -2,17 +2,25 @@
 //  ContentView.swift
 //  Calculator
 //
-//  Created by Megan Marshall on 7/10/20.
-//  Copyright © 2020 Megan Marshall. All rights reserved.
-//
 
 import SwiftUI
 
 struct ContentView: View {
+   
     var body: some View {
-        Text("Hello, World!")
+         ZStack {
+           Text("Hello World")
+            
+           //Background
+           Rectangle()
+              
+           
+           Rectangle()
+               .rotationEffect(Angle(degrees: 45))
+       }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ Calculator/ViewController.swift
@@ -0,0 +1,300 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//
+
+import SwiftUI
+
+struct ViewController: View {
+    @State private var currentNum = 0
+    @State private var done = false
+    @State private var operator1 = "+"
+    @State private var prevNum1 = 0
+    @State private var operator2 = "+"
+    @State private var prevNum2 = 0
+    @State private var error = false
+    
+    var body: some View {
+        ZStack {
+            
+            //Background
+            Rectangle()
+                .foregroundColor(.blue)
+                .edgesIgnoringSafeArea(.all)
+            
+            Rectangle()
+                .foregroundColor(.green)
+                .rotationEffect(Angle(degrees: 135))
+                .edgesIgnoringSafeArea(.all)
+            
+            //Content
+            VStack {
+                Spacer()
+                
+                //Title
+                Text("Calculator")
+                    .bold()
+                    .foregroundColor(.white)
+                    .scaleEffect(3)
+                
+                Spacer()
+                
+                //Current Result
+                if(done) {
+                    Text(String(prevNum1))
+                }
+                else{
+                    Text(String(currentNum))
+                }
+                
+                Spacer()
+                
+                //Buttons
+                VStack{
+                    Spacer()
+                    
+                    HStack {
+                        Spacer()
+                        
+                        Button(action: {
+                            //TODO
+                        }) {
+                
+                            Text("+")
+                                .bold()
+                                .padding(.all, 10)
+                                .foregroundColor(.white)
+                                .background(Color.black)
+                        }
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            //TODO
+                            }) {
+                            Text("-")
+                                .bold()
+                                .padding(.all, 10)
+                                .foregroundColor(.white)
+                                .background(Color.black)
+                        }
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            //TODO
+                            }) {
+                            Text("*")
+                                .bold()
+                                .padding(.all, 10)
+                                .foregroundColor(.white)
+                                .background(Color.black)
+                        }
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            //TODO
+                            }) {
+                            Text("/")
+                                .bold()
+                                .padding(.all, 10)
+                                .foregroundColor(.white)
+                                .background(Color.black)
+                        }
+                        
+                        Spacer()
+                        
+                    }
+                    
+                    Spacer()
+
+                    HStack {
+                        Spacer()
+                        
+                        Button(action: {
+                                //TODO
+                                }) {
+                    
+                                Text("7")
+                                    .bold()
+                                    .padding(.all, 10)
+                                    .foregroundColor(.white)
+                                    .background(Color.black)
+                        }
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                                //TODO
+                                }) {
+                                Text("8")
+                                    .bold()
+                                    .padding(.all, 10)
+                                    .foregroundColor(.white)
+                                    .background(Color.black)
+                        }
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                                //TODO
+                                }) {
+                                Text("9")
+                                    .bold()
+                                    .padding(.all, 10)
+                                    .foregroundColor(.white)
+                                    .background(Color.black)
+                        }
+                        
+                        Spacer()
+                        
+                        }
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Spacer()
+                        
+                        Button(action: {
+                                //TODO
+                                }) {
+                    
+                                Text("4")
+                                    .bold()
+                                    .padding(.all, 10)
+                                    .foregroundColor(.white)
+                                    .background(Color.black)
+                        }
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                                //TODO
+                                }) {
+                                Text("5")
+                                    .bold()
+                                    .padding(.all, 10)
+                                    .foregroundColor(.white)
+                                    .background(Color.black)
+                        }
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                                //TODO
+                                }) {
+                                Text("6")
+                                    .bold()
+                                    .padding(.all, 10)
+                                    .foregroundColor(.white)
+                                    .background(Color.black)
+                        }
+                        
+                        Spacer()
+                        
+                    }
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Spacer()
+                        
+                        Button(action: {
+                            //TODO
+                            }) {
+                
+                            Text("1")
+                                .bold()
+                                .padding(.all, 10)
+                                .foregroundColor(.white)
+                                .background(Color.black)
+                        }
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            //TODO
+                            }) {
+                            Text("2")
+                                .bold()
+                                .padding(.all, 10)
+                                .foregroundColor(.white)
+                                .background(Color.black)
+                        }
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            //TODO
+                            }) {
+                            Text("3")
+                                .bold()
+                                .padding(.all, 10)
+                                .foregroundColor(.white)
+                                .background(Color.black)
+                        }
+                        
+                        Spacer()
+                        
+                    }
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Spacer()
+                        
+                        Button(action: {
+                            //TODO
+                            }) {
+                
+                            Text("0")
+                                .bold()
+                                .padding(.all, 10)
+                                .foregroundColor(.white)
+                                .background(Color.black)
+                        }
+                        
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            //TODO
+                            }) {
+                            Text(".")
+                                .bold()
+                                .padding(.all, 10)
+                                .foregroundColor(.white)
+                                .background(Color.black)
+                        }
+                        
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            //TODO
+                            }) {
+                            Text("=")
+                                .bold()
+                                .padding(.all, 10)
+                                .foregroundColor(.white)
+                                .background(Color.black)
+                        }
+                        
+                        
+                        Spacer()
+                        
+                    }
+                }
+                Spacer()
+            }
+        }
+    }
+}
+
+struct ViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewController()
+    }
+}
